70b871e933b227a2d30b8393a2cd1cafe9f67b7f
adhokshaj-k <144054981+adhokshaj-k@users.noreply.github.com>
2024-10-15 19:20:11 +0530

    Solved the error of outpu

---
+++ subd/subd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#need input
+domain=$1
+
+
+curl -s https://crt.sh/\?q\=\%.$domain\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $domain-crt.txt
+
+sublist3r -d $domain -v > $domain-sublister.txt
+
+cat $domain-crt.txt | ~/go/bin/anew $domain-sublister.txt
+
+cat $domain-sublister.txt | htppx -sc > httpx_domains.txt
+
+
