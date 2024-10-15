1afd498643d30011b3407e0efb6b4d5c0c513641
adhokshaj-k <144054981+adhokshaj-k@users.noreply.github.com>
2024-10-15 19:25:15 +0530

    Solved the error of output

--- subd/subd.sh
+++ subd/subd.sh
@@ -6,7 +6,7 @@ domain=$1
 
 curl -s https://crt.sh/\?q\=\%.$domain\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $domain-crt.txt
 
-sublist3r -d $domain -v > $domain-sublister.txt
+sublist3r -d $domain -v -o $domain-sublister.txt
 
 cat $domain-crt.txt | ~/go/bin/anew $domain-sublister.txt
 
