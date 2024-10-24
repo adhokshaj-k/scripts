c7430456b374c8606dcc1e1951c77315e17a9714
adhokshaj-k <144054981+adhokshaj-k@users.noreply.github.com>
2024-10-24 10:47:05 +0530

    Reverse Eng Scripts

---
+++ revEng/dec_txt.py
@@ -0,0 +1,13 @@
+#this is for decoding text in encoded format 
+#it for reverse eng scripts
+
+
+encoded_flag = open("enc").read()
+flag = ""
+for i in range(0, len(encoded_flag)):
+    character1 = chr((ord(encoded_flag[i]) >> 8))
+    character2 = chr(encoded_flag[i].encode('utf-16be')[-1])
+    flag += character1
+    flag += character2
+
+print("Flag: " + flag)
