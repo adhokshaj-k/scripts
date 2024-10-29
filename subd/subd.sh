df31762dba8d5da28148e6a2ab918216c814ed5b
adhokshaj-k <144054981+adhokshaj-k@users.noreply.github.com>
2024-10-29 23:09:39 +0530

    added filtering feature

--- subd/subd.sh
+++ subd/subd.sh
@@ -3,14 +3,18 @@
 #need input
 domain=$1
 cd recon
-mkdir $domain
+mkdir $domain-subdout
 
-curl -s https://crt.sh/\?q\=\%.$domain\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $domain/$domain-crt.txt
+curl -s https://crt.sh/\?q\=\%.$domain\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $domain-subdout/$domain-crt.txt
 
-sublist3r -d $domain -v -o $domain/$domain-sublister.txt
+sublist3r -d $domain -v -o $domain-subdout/$domain-sublister.txt
 
-cat $domain/$domain-crt.txt | ~/go/bin/anew $domain/$domain-sublister.txt
+cat $domain-subdout/$domain-crt.txt | ~/go/bin/anew $domain-subdout/$domain-sublister.txt
 
-cat $domain/$domain-sublister.txt | httpx -sc > $domain/$domain-httpx.txt
+cat $domain-subdout/$domain-sublister.txt | httpx -sc > $domain-subdout/$domain-httpx.txt
+
+grep "200" $domain-subdout/$domain-httpx.txt > $domain-subdout/alive.txt 
+grep "403" $domain-subdout/$domain-httpx.txt > $domain-subdout/forbidden.txt 
+
 
 
