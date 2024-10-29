71dc9d784e194c016b2314f6823bc3424a7a9dde
root <root@WIN10.>
2024-10-29 23:24:04 +0530

    added follow-redirection feature

--- subd/subd.sh
+++ subd/subd.sh
@@ -12,7 +12,7 @@ sublist3r -d $domain -v -o $domain-subdout/$domain-sublister.txt
 
 cat $domain-subdout/$domain-crt.txt | ~/go/bin/anew $domain-subdout/$domain-sublister.txt
 
-cat $domain-subdout/$domain-sublister.txt | httpx -sc > $domain-subdout/$domain-httpx.txt
+cat $domain-subdout/$domain-sublister.txt | httpx -sc -fr > $domain-subdout/$domain-httpx.txt
 
 grep "200" $domain-subdout/$domain-httpx.txt > $domain-subdout/alive.txt 
 grep "403" $domain-subdout/$domain-httpx.txt > $domain-subdout/forbidden.txt 
