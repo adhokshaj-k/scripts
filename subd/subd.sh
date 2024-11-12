8bb922139940c3a7ce558f466e2c5594fc06160a
adhokshaj-k <144054981+adhokshaj-k@users.noreply.github.com>
2024-11-12 16:17:54 +0530

    changed the subdomain enumeration tool

--- subd/subd.sh
+++ subd/subd.sh
@@ -10,11 +10,11 @@ mkdir $domain-subdout
 
 curl -s https://crt.sh/\?q\=\%.$domain\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $domain-subdout/$domain-crt.txt
 
-sublist3r -d $domain -v -o $domain-subdout/$domain-sublister.txt
+subfinder -d $domain -rl 5 -v -o $domain-subdout/$domain-sub.txt
 
-cat $domain-subdout/$domain-crt.txt | ~/go/bin/anew $domain-subdout/$domain-sublister.txt
+cat $domain-subdout/$domain-crt.txt | ~/go/bin/anew $domain-subdout/$domain-sub.txt
 
-cat $domain-subdout/$domain-sublister.txt | httpx -sc -fr > $domain-subdout/$domain-httpx.txt
+cat $domain-subdout/$domain-sub.txt | httpx -sc -fr > $domain-subdout/$domain-httpx.txt
 
 grep "200" $domain-subdout/$domain-httpx.txt > $domain-subdout/alive.txt 
 grep "403" $domain-subdout/$domain-httpx.txt > $domain-subdout/forbidden.txt 
